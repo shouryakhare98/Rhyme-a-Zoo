fa1db84228e9325d5ecb2d19c7d12f45f4bda36f
shouryakhare98 <shouryakhare98@gmail.com>
2019-01-19 13:39:54 -0500

    Bank page created and transitions applied.

--- app/src/main/java/com/example/shouryakhare/rhyme_a_zoo/BankActivity.java
+++ app/src/main/java/com/example/shouryakhare/rhyme_a_zoo/BankActivity.java
@@ -1,7 +1,12 @@
 package com.example.shouryakhare.rhyme_a_zoo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class BankActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class BankActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bank);
+
+        final ImageView home = findViewById(R.id.bankActivity_home);
+        home.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(BankActivity.this, MainActivity.class);
+                BankActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/shouryakhare/rhyme_a_zoo/MainActivity.java
+++ app/src/main/java/com/example/shouryakhare/rhyme_a_zoo/MainActivity.java
@@ -17,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView bank = findViewById(R.id.mainActivity_bank);
+        bank.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, BankActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 
     @Override
