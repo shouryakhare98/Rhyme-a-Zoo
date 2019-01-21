b91fee318ea124c43c6b82ad8564b3b0b35b48fe
shouryakhare98 <shouryakhare98@gmail.com>
2019-01-21 13:41:17 -0500

    Bank page updated with total coins transition.

--- app/src/main/java/com/example/shouryakhare/rhyme_a_zoo/BankActivity.java
+++ app/src/main/java/com/example/shouryakhare/rhyme_a_zoo/BankActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,11 +17,22 @@ public class BankActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //Retrive coin values with shared preferences
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bank);
+
+        TextView noCoins = findViewById(R.id.bankActivity_nocoins);
+        ImageView emote = findViewById(R.id.bankActivity_worriedface);
+        final Button repeat = findViewById(R.id.bankActivity_repeat);
+        final Button home = findViewById(R.id.bankActivity_home);
+        final TextView numTotalGoldCoins = findViewById(R.id.bankActivity_numgold);
+        final TextView numTotalSilverCoins = findViewById(R.id.bankActivity_numsilver);
+        final TextView plusSign = findViewById(R.id.bankActivity_plus);
+        final TextView earnedTotal = findViewById(R.id.bankActivity_earnedtotal);
+
+        //Retrieve coin values with shared preferences
         SharedPreferences pref = getSharedPreferences("MyPref", 0);
         int currentCoins = pref.getInt("currentCoins", 0); //0 is default value if currentCoins does not exist
         int totalCoins = pref.getInt("totalCoins", 0); //0 is default value if totalCoins does not exist
-        int coins = 0; //get coins here
 
         int silver = currentCoins % 2;
         currentCoins = currentCoins - silver;
@@ -33,19 +45,31 @@ public class BankActivity extends AppCompatActivity {
         gold = gold % 5;
         int one = gold;
 
-        if (coins >= 140) {
+        final int totalSilver = totalCoins % 2;
+        final int totalGold = totalCoins / 2;
+        numTotalGoldCoins.setText(String.valueOf(totalGold));
+        if (totalSilver != 0) {
+            numTotalSilverCoins.setText(String.valueOf(totalSilver));
+            plusSign.setText("+");
+        }
+
+        if (currentCoins >= 140) {
             // hide text view that says no coins
-        } if (coins <= 0) {
+            noCoins.invalidate();
+            emote.invalidate();
+        } if (currentCoins <= 0) {
             //show no coins text
         } else {
             //display coin piles
+            noCoins.invalidate();
+            emote.invalidate();
         }
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bank);
+        numTotalGoldCoins.setVisibility(View.INVISIBLE);
+        plusSign.setVisibility(View.INVISIBLE);
+        numTotalSilverCoins.setVisibility(View.INVISIBLE);
+        earnedTotal.setVisibility(View.INVISIBLE);
 
-
-        final Button home = findViewById(R.id.bankActivity_home);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,10 +78,6 @@ public class BankActivity extends AppCompatActivity {
             }
         });
 
-        TextView noCoins = findViewById(R.id.bankActivity_nocoins);
-        ImageView emote = findViewById(R.id.bankActivity_worriedface);
-
-        final Button repeat = findViewById(R.id.bankActivity_repeat);
         repeat.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 repeat.setEnabled(false);
@@ -76,6 +96,19 @@ public class BankActivity extends AppCompatActivity {
                         animate.setFillAfter(true);
                         currentView.startAnimation(animate);
 
+                        numTotalGoldCoins.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_in));
+                        numTotalGoldCoins.setVisibility(View.VISIBLE);
+                        earnedTotal.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_in));
+                        earnedTotal.setVisibility(View.VISIBLE);
+
+                        if (totalSilver != 0) {
+                            numTotalSilverCoins.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_in));
+                            numTotalSilverCoins.setVisibility(View.VISIBLE);
+
+                            plusSign.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_in));
+                            plusSign.setVisibility(View.VISIBLE);
+                        }
+
                         coinsTotal.start();
                     }
                 });
@@ -90,6 +123,19 @@ public class BankActivity extends AppCompatActivity {
 
                         repeat.setEnabled(true);
                         home.setEnabled(true);
+
+                        numTotalGoldCoins.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_out));
+                        numTotalGoldCoins.setVisibility(View.INVISIBLE);
+                        earnedTotal.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_out));
+                        earnedTotal.setVisibility(View.INVISIBLE);
+
+                        if (totalSilver != 0) {
+                            numTotalSilverCoins.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_out));
+                            numTotalSilverCoins.setVisibility(View.INVISIBLE);
+
+                            plusSign.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_out));
+                            plusSign.setVisibility(View.INVISIBLE);
+                        }
                     }
                 });
             }
