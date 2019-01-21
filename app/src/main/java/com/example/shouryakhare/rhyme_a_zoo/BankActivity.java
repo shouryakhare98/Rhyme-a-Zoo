e76536528fa330c820075467f18451fefd5c8595
DomKynkor <domkynkor@gmail.com>
2019-01-21 13:10:50 -0500

    Coin Display

--- app/src/main/java/com/example/shouryakhare/rhyme_a_zoo/BankActivity.java
+++ app/src/main/java/com/example/shouryakhare/rhyme_a_zoo/BankActivity.java
@@ -9,31 +9,50 @@ import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class BankActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        int coins = 0; //get coins here
-
-        int silver = coins % 2;
-        coins = coins - silver;
-        int gold = coins / 2;
-        int twenty = gold / 20;
-        gold = gold % 20;
-        int ten = gold /10;
-        gold = gold % 10;
-        int five = gold / 5;
-        gold = gold % 5;
-        int one = gold;
+        int coins = 1; //get coins here
 
         if (coins >= 140) {
-            // hide text view that says no coins
+            //
         } if (coins <= 0) {
             //show no coins text
         } else {
+            //coin logic
+            int silver = coins % 2;
+            coins = coins - silver;
+            int gold = coins / 2;
+            int twenty = gold / 20;
+            gold = gold % 20;
+            int ten = gold /10;
+            gold = gold % 10;
+            int five = gold / 5;
+            gold = gold % 5;
+            int one = gold;
+
             //display coin piles
+            LinearLayout linearLayout= new LinearLayout(this);
+            linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+            linearLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
+                    LayoutParams.MATCH_PARENT));
+
+            ImageView imageView = new ImageView(this);
+
+            int id = getResources().getIdentifier("goldcoin", "drawable", getPackageName());
+            imageView.setImageResource(id);
+
+            imageView.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
+                    LayoutParams.WRAP_CONTENT));
+            linearLayout.addView(imageView);
+            setContentView(linearLayout);
         }
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bank);
@@ -49,6 +68,11 @@ public class BankActivity extends AppCompatActivity {
 
         TextView noCoins = findViewById(R.id.bankActivity_nocoins);
         ImageView emote = findViewById(R.id.bankActivity_worriedface);
+
+
+
+
+
 
         final Button repeat = findViewById(R.id.bankActivity_repeat);
         repeat.setOnClickListener(new View.OnClickListener() {
@@ -90,4 +114,5 @@ public class BankActivity extends AppCompatActivity {
 
         repeat.performClick();
     }
+
 }
