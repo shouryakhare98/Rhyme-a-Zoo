d37c58748b934186922cfa2a425588f672cfb938
Sai Tadikonda <saitadikonda48@gmail.com>
2019-02-08 13:43:10 -0500

    "Rhyme Menu"

--- app/src/main/java/com/example/shouryakhare/rhyme_a_zoo/RhymeMenuActivity.java
+++ app/src/main/java/com/example/shouryakhare/rhyme_a_zoo/RhymeMenuActivity.java
@@ -3,8 +3,12 @@ package com.example.shouryakhare.rhyme_a_zoo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 public class RhymeMenuActivity extends AppCompatActivity {
 
@@ -13,7 +17,9 @@ public class RhymeMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rhyme_menu);
 
+
         final Button home = findViewById(R.id.rhymeActivity_home);
+
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +28,7 @@ public class RhymeMenuActivity extends AppCompatActivity {
                 RhymeMenuActivity.this.startActivity(myIntent);
             }
         });
+
+
     }
 }
